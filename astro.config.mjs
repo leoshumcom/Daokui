991008d6a1c96efb46a12c259fa2ab473dcb6be1
leoshumcom <leoshum.com@gmail.com>
2026-07-07 10:01:05 +0800

    fix: 移除sitemap插件，初始部署完成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://daokui.com',
   integrations: [
     tailwind(),
-    sitemap(),
   ],
 });
